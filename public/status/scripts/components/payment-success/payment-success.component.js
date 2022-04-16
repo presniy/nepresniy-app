9d3f15aef4c0b3af3d5fe2ede55bec7922aefbb8
Anton Kalashnikov <tohasan@yandex.ru>
2022-04-16 19:29:55 +0300

    status: loaded the info about the product

--- public/status/scripts/components/payment-success/payment-success.component.js
+++ public/status/scripts/components/payment-success/payment-success.component.js
@@ -6,7 +6,12 @@ export class PaymentSuccessComponent {
         const elWaitStatus = document.querySelector('.content_type_wait-status');
         elWaitStatus.classList.add('hidden');
 
+        const { recipeUrl, product } = gift;
+
+        elPaymentSuccess.querySelector('.product__offer-name').textContent = product.offerName;
+        elPaymentSuccess.querySelector('.product__bonus-recipe-name').textContent = product.bonusRecipeName;
+
         const elLink = elPaymentSuccess.querySelector('.recipe-download-link');
-        elLink.setAttribute('href', gift.recipeUrl);
+        elLink.setAttribute('href', recipeUrl);
     }
 }
